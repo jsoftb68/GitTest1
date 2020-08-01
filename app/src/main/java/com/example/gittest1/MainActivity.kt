c56149d9db92f65966a4fb4a79e83e66192c9aee
jsoftb68 <juergen.boczek@gmail.com>
2020-08-01 14:07:33 +0200

    Zähler hinzugefügt

--- app/src/main/java/com/example/gittest1/MainActivity.kt
+++ app/src/main/java/com/example/gittest1/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.gittest1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var counter: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_count.setOnClickListener {
+            counter++
+            tv_output.text = counter.toString()
+        }
     }
 }
