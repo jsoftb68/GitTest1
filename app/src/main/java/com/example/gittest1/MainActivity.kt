c738ac774766702410da908627ec9747fd4322ea
Karl Knapp <clajubo@arcor.de>
2020-08-15 16:00:11 +0200

    Karl Knapp hat Resetwert auf 20 geändert

--- app/src/main/java/com/example/gittest1/MainActivity.kt
+++ app/src/main/java/com/example/gittest1/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         btn_count_reset.setOnClickListener {
-            counter = 10
+            counter = 20
             tv_output.text = counter.toString()
         }
     }
